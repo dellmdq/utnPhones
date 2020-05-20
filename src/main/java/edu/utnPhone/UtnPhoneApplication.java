8431104ee1f83da11762522d25198b5d0dc6b019
dellmdq <erikdell@gmail.com>
2020-05-20 17:35:15 -0300

    commit inicial para flor

---
+++ src/main/java/edu/utnPhone/UtnPhoneApplication.java
@@ -0,0 +1,13 @@
+package edu.utnPhone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utnPhone/UtnPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utnPhone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
